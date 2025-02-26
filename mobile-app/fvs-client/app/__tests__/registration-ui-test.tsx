952829c2e1dd905e2bd4f2f96f4742d1dbd218dd
Umendya Dias <dev.mendy.das@gmail.com>
2025-02-26 07:56:55 +0530

    partial(mobile): working on the registration ui test

--- mobile-app/fvs-client/app/__tests__/registration-ui-test.tsx
+++ mobile-app/fvs-client/app/__tests__/registration-ui-test.tsx
@@ -1,10 +1,11 @@
 import { renderRouter, screen, userEvent } from "expo-router/testing-library";
 import LoginPage from "..";
 import RegisterDetailsPage from "../register/details";
+import { GENDERS, Voter } from "@/models/voter";
 
 let routes = {
   index: jest.fn(() => <LoginPage />),
-  "register/details": jest.fn(() => <RegisterDetailsPage />),
+  "/register/details": jest.fn(() => <RegisterDetailsPage />),
 };
 
 jest.mock("expo-font");
@@ -18,5 +19,26 @@ describe("Basic user registration flow", function () {
     await driver.press(registerBtn);
     expect(screen).toHavePathname("/register/details");
   });
-  it("completing and submitting the registration form should save the voter correctly.", async function () {});
+  it("completing the registration form correctly should display a success page", async function () {
+    renderRouter(routes, { initialUrl: "/register/details" });
+    // get the field elements
+    const driver = userEvent.setup();
+    const nicField = screen.getByRole("text", {
+      name: "National Identification Number",
+    });
+    const nameField = screen.getByRole("text", { name: "Full name" });
+    const dobField = screen.getByRole("text", { name: "Date of birth" });
+    const genderField = screen.getByRole("text", { name: "Gender" });
+    const emailField = screen.getByRole("text", { name: "Email address" });
+    // get the register button
+    const registerButton = screen.getByRole("button", { name: "Register!" });
+    // the voter entity to compare it to
+    const voter: Voter = {
+      nic: "941993850V",
+      fullname: "Umendya Ranmal Dias",
+      dob: new Date("1994-07-17"),
+      email: "u.ranmal.dias@gmail.com",
+      gender: 
+    };
+  });
 });
--- mobile-app/fvs-client/models/voter.ts
+++ mobile-app/fvs-client/models/voter.ts
@@ -1,1 +1,9 @@
 export const GENDERS = ["Male", "Female"];
+
+export type Voter = {
+  nic: string;
+  fullname: string;
+  dob: Date;
+  gender: string;
+  email: string;
+};
