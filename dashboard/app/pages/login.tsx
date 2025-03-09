2fd092285409d692b2d1dbce9007182a3092b5d6
Umendya Dias <dev.mendy.das@gmail.com>
2025-03-09 10:26:18 +0530

    test(dashboard): admin login page renders correctly

---
+++ dashboard/app/pages/login.tsx
@@ -0,0 +1,9 @@
+import { Box, Center } from "@chakra-ui/react";
+
+export default function LoginPage() {
+  return (
+    <Center width="dvw" height="dvh">
+      <Box>Hello, world!</Box>
+    </Center>
+  );
+}
---
+++ dashboard/vite.config.ts
@@ -0,0 +1,13 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setup-test.ts",
+  },
+});
