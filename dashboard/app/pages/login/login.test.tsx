c3ecfb80a96b56a8a97acf0f6ce6db7cfc8556d5
Umendya Dias <dev.mendy.das@gmail.com>
2025-03-10 06:37:41 +0530

    feat(dashboard): login form validation

--- dashboard/app/pages/login/login.test.tsx
+++ dashboard/app/pages/login/login.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react";
+import { act, screen, waitFor } from "@testing-library/react";
 import render from "~/test-utils/renderer";
 import "@testing-library/jest-dom";
 import { createRoutesStub } from "react-router";
@@ -24,21 +24,35 @@ describe("Login UI", function () {
     expect(screen.getByRole("button", { name: "Login" })).toBeInTheDocument();
   });
 
-  it("should render an error message if email address and password are empty when logging in", function () {
+  it("should render an error message if email address and password are empty when logging in", async function () {
     const RouteStub = createRoutesStub([
       {
         path: "/",
         Component: LoginPage,
+        action() {
+          return {
+            status: "error",
+            intent: undefined,
+            initialValue: {},
+            error: {
+              email: ["Email address cannot be empty."],
+              password: ["Password cannot be empty."],
+            },
+            state: undefined,
+            fields: ["email", "password"],
+          };
+        },
       },
     ]);
 
     render(<RouteStub initialEntries={["/"]} />);
 
     const loginButton = screen.getByRole("button", { name: "Login" });
-    loginButton.click();
-    expect(
-      screen.getByText("Email address cannot be empty.")
-    ).toBeInTheDocument();
-    expect(screen.getByText("Password cannot be empty.")).toBeInTheDocument();
+    act(() => {
+      loginButton.click();
+    });
+    await waitFor(() => screen.findByText("Email address cannot be empty."));
+    await waitFor(() => screen.findByText("Password cannot be empty."));
   });
+  // TODO: test for invalid email message
 });
--- dashboard/app/pages/login/login.tsx
+++ dashboard/app/pages/login/login.tsx
@@ -11,9 +11,49 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { Form } from "react-router";
+import { Form, redirect, type ActionFunctionArgs } from "react-router";
+import { z } from "zod";
+import { getZodConstraint, parseWithZod } from "@conform-to/zod";
+import { useActionData } from "react-router";
+import { useForm } from "@conform-to/react";
+import type { Route } from "../../+types/root";
+import { useEffect } from "react";
+
+const loginSchema = z.object({
+  email: z.string({ required_error: "Email address cannot be empty." }).email(),
+  password: z.string({ required_error: "Password cannot be empty." }).max(50),
+});
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+
+  const submission = parseWithZod(formData, { schema: loginSchema });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  // TODO: implement check login ok logic
+  return redirect("/dashboard");
+}
 
 export default function LoginPage() {
+  const actionData = useActionData<typeof action>();
+  const [form, fields] = useForm({
+    lastResult: actionData,
+    constraint: getZodConstraint(loginSchema),
+    id: "loginFormId",
+    defaultValue: {
+      email: "",
+      password: "",
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: loginSchema });
+    },
+  });
+
   return (
     <Flex
       width="full"
@@ -21,7 +61,83 @@ export default function LoginPage() {
       alignItems="center"
       justifyContent="center"
     >
-      <LoginForm />
+      <Box
+        backgroundColor="white"
+        display="flex"
+        width="full"
+        height="full"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <VStack padding="8">
+          <Heading color="primary" size="5xl">
+            FVS
+          </Heading>
+          <Heading color="primary" size="4xl">
+            Welcome back!
+          </Heading>
+          <Text color="primary">
+            Please enter your username and password to access your account.
+          </Text>
+          <Form
+            id={form.id}
+            method="post"
+            onSubmit={form.onSubmit}
+            style={{ width: "100%" }}
+          >
+            <Stack marginY="4" gap="4" width="full" padding="4">
+              <Field.Root
+                invalid={fields.email.errors && fields.email.errors.length > 0}
+              >
+                <Field.Label htmlFor={fields.email.id} color="primary">
+                  Email address:
+                </Field.Label>
+                <Input
+                  id={fields.email.id}
+                  name={fields.email.name}
+                  type="email"
+                  color="primary"
+                  defaultValue={fields.email.initialValue}
+                  placeholder="Username"
+                />
+                <Field.ErrorText id={fields.email.errorId}>
+                  {fields.email.errors}
+                </Field.ErrorText>
+              </Field.Root>
+              <Field.Root
+                invalid={
+                  fields.password.errors && fields.password.errors.length > 0
+                }
+              >
+                <Field.Label htmlFor={fields.password.id} color="primary">
+                  Password:
+                </Field.Label>
+                <Input
+                  id={fields.password.id}
+                  name={fields.password.name}
+                  type="password"
+                  color="primary"
+                  defaultValue={fields.password.initialValue}
+                  placeholder="Password"
+                />
+                <Field.ErrorText color="red" id={fields.password.errorId}>
+                  {fields.password.errors}
+                </Field.ErrorText>
+              </Field.Root>
+            </Stack>
+            <Center>
+              <Button
+                variant="outline"
+                color="primary"
+                _hover={{ color: "secondary" }}
+                type="submit"
+              >
+                Login
+              </Button>
+            </Center>
+          </Form>
+        </VStack>
+      </Box>
       <Center
         backgroundColor="secondary"
         width="full"
@@ -31,51 +147,5 @@ export default function LoginPage() {
         <Image src="/fingerprint.jpg" width="full" />
       </Center>
     </Flex>
-  );
-}
-
-function LoginForm() {
-  return (
-    <Box
-      backgroundColor="white"
-      display="flex"
-      width="full"
-      height="full"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <VStack padding="8">
-        <Heading color="primary" size="5xl">
-          FVS
-        </Heading>
-        <Heading color="primary" size="4xl">
-          Welcome back!
-        </Heading>
-        <Text color="primary">
-          Please enter your username and password to access your account.
-        </Text>
-        <Form style={{ width: "100%" }}>
-          <Stack marginY="4" gap="4" width="full" padding="4">
-            <Field.Root required>
-              <Field.Label color="primary">Email address:</Field.Label>
-              <Input placeholder="Username" />
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label color="primary">Password:</Field.Label>
-              <Input placeholder="Password" />
-            </Field.Root>
-          </Stack>
-          <Center>
-            <Button
-              variant="outline"
-              color="primary"
-              _hover={{ color: "secondary" }}
-            >
-              Login
-            </Button>
-          </Center>
-        </Form>
-      </VStack>
-    </Box>
   );
 }
