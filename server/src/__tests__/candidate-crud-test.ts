5b04ed2526044406a4c93671de0901de8e60084f
Umendya Dias <dev.mendy.das@gmail.com>
2025-03-12 03:13:15 +0530

    test(server): election and candidate tests

---
+++ server/src/__tests__/candidate-crud-test.ts
@@ -0,0 +1,12 @@
+import { describe } from "@jest/globals";
+import { it } from "node:test";
+
+describe("Candidate CRUD tests", function () {
+  it.todo("should promote a voter to a candidate by an admin");
+
+  it.todo("should demote a candidate by an admin");
+
+  it.todo("should allow a voter to apply for a candidacy for an election");
+
+  it.todo("should retrieve all candidates for an election");
+});
---
+++ server/src/__tests__/election-crud-test.ts
@@ -0,0 +1,10 @@
+import { describe, it } from "@jest/globals";
+
+describe("Election CRUD tests", function () {
+  it.todo("should create an election");
+  it.todo("should retrieve an election");
+  it.todo("should update an election");
+  it.todo("should delete an election");
+  it.todo("should retrieve all elections");
+  it.todo("should retrieve the election given the date");
+});
